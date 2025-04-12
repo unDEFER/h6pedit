c915a472d85669375858d8c4dbc493b0af80c122
unDEFER <undefer@gmail.com>
2025-04-12 09:07:50 +0300

    autocommit on successful build

--- src/h6pedit/tick.d
+++ src/h6pedit/tick.d
@@ -847,7 +847,7 @@ void process_mask2_editor_keys(SDL_Event event)
 
                 dotx = cast(ubyte) (dotx_ - (5-dot_by_line[doty].length)/2);
 
-                writefln("dotx = %s, dotx_ = %s", dotx, dotx_);
+                writefln("dotx = %s (%s), dotx_ = %s", dotx, dot_by_line[doty].length, dotx_);
                 if (dotx >= dot_by_line[doty].length)
                     dotx = cast(ubyte) (dotx > 200 ? dot_by_line[doty].length-1 : 0);
 
