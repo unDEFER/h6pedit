83002f5cb04bbb89ea8c3293bbd1782a07cfe965
unDEFER <undefer@gmail.com>
2025-05-13 16:26:37 +0300

    autocommit on successful build

--- src/h6pedit/tick.d
+++ src/h6pedit/tick.d
@@ -927,8 +927,13 @@ void process_mask2_editor_keys(SDL_Event event)
         edited_form = cast(ubyte) p.forms.length;
         form_dots.length = 0;
 
-        float ERR = apply_brush(brush);
-        writefln("%sx%s ERR = %s", select.x, select.y, ERR);
+        float merr = apply_brush(brush, true) < 0.01;
+        if ( merr < 0.01 )
+        {
+            apply_brush(brush);
+        }
+
+        writefln("%sx%s MERR = %s", select.x, select.y, merr);
     }
 
     if (event.key.keysym.scancode == SDL_SCANCODE_L)
