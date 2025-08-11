fdc4c6acb2e2157ef706d7ff61e5cd9336b6c3c3
unDEFER <undefer@gmail.com>
2025-08-11 06:28:43 +0300

    autocommit on successful build

--- src/hexpict/hyperpixel.d
+++ src/hexpict/hyperpixel.d
@@ -745,7 +745,7 @@ BitArray *hyperpixel(int w, ubyte[12] form12, ubyte rotate, bool _debug = false)
                 }
             }
 
-            if (minDist > 0.02)
+            if (minDist > 0.2)
             {
                 writefln("minDist = %s, p = %s, bestP = %s", minDist, p, bestP);
             }
